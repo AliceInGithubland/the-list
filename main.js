904fcce20c70dee76261271c176216ce0e56f24b
alicepetzinger <alice.petzinger@gmail.com>
2021-10-18 09:14:00 +0200

    add textareas

---
+++ main.js
@@ -0,0 +1,3 @@
+import { createTitleComponent } from "./titleComponent.js";
+const titleComponent = createTitleComponent();
+document.body.append(titleComponent);
---
+++ titleComponent.js
@@ -0,0 +1,20 @@
+import { createElement } from "./element.js";
+export function createTitleComponent() {
+  const createTitleComponent = createElement("h1", {
+    textcontent: "Mood & Meme",
+    className: "title",
+  });
+
+  return createTitleComponent;
+}
+
+h1{
+    font-family: 'Roboto', sans-serif;
+  }
+  
+  h2 {
+    font: italic small-caps normal 13px/150% 'Kalam', cursive;
+  }
+
+
+let = document.createElement("ul", { is: "expanding-list" });
